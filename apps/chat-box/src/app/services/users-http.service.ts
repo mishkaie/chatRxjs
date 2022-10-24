ea020c2c719ffbb3d6e4da6521723593e905d71c
Mikheil Zhvania <m_zhvania2@cu.edu.ge>
2022-10-24 11:35:11 +0400

    remove unused code in service

--- apps/chat-box/src/app/services/users-http.service.ts
+++ apps/chat-box/src/app/services/users-http.service.ts
@@ -5,10 +5,8 @@ import { Observable, shareReplay, Subject , BehaviorSubject} from 'rxjs';
 import { User } from '../user/user.model';
 import { Chat } from '../chat/chat.model';
 
-// export const usersSubj$ = new ReplaySubject<User[]>();
 export const usersSubj$ = new Subject<User[]>();
 export const messagesSubj$ =  new BehaviorSubject<Chat[]>([]);
-// export const currentUser$ = new Subject<User>();
 
 
 @Injectable({
